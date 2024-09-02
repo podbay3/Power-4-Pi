7f496ef6c0b804f36e54bde21fef5dcc198d045f
podbay3 <info@podbay3.com>
2024-09-02 16:48:24 -0500

    Update ATX-Watchdog_install.sh

--- ATX-Power_Management_Shield/Rev_1.0/Scripts/ATX-Watchdog_install.sh
+++ ATX-Power_Management_Shield/Rev_1.0/Scripts/ATX-Watchdog_install.sh
@@ -2,16 +2,12 @@ sudo mkdir /usr/local/bin/ATX-Watchdog
 echo 'import RPi.GPIO as GPIO
 import os
 import sys
-import smbus
 import time
 
 GPIO.setmode(GPIO.BCM)
 
 pulseStart = 0.0
 SHUTDOWN = 24               #pin 18
-ATX_WATCHDOG_ADDRESS = 0x5A #I2C address
-BOOT_OK_COMMAND = 0x83      #Set Boot Ok process
-BOOT_OK = 0x01              #Signal that we booted up okay
 REBOOTPULSEMINIMUM = 0.2    #reboot pulse signal should be at least this long
 REBOOTPULSEMAXIMUM = 0.6    #reboot pulse signal should be at most this long
 
@@ -20,10 +16,6 @@ print ("== ATX-PSU_startup: Initializing GPIO")
 GPIO.setup(SHUTDOWN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 try:
-    print ("\n== Signalling Boot Ok\n")
-    bus = smbus.SMBus(1)
-    bus.write_byte_data(ATX_WATCHDOG_ADDRESS, BOOT_OK_COMMAND, BOOT_OK)
-    bus.close()
 
     while True:
         print ("\n== Waiting for shutdown pulse\n")
